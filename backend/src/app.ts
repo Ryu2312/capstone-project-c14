742e60353b1d11c4edbfa226bfbfb904448c6c93
ryu <alessiasophia23@gmail.com>
2024-11-28 15:18:05 -0500

    s

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+/* import express from 'express';
+import path from 'path';
+
+export const app = express();
+
+app.use(express.json());
+const distPath = path.join(path.resolve(), "frontend", "dist");
+app.use(express.static(distPath));
+
+
+app.get("/", (_req, res) => {
+    const __dirname = path.join(path.resolve(), "frontend", "dist");
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, "index.html"));
+}); */
